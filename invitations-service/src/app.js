3f736947861963e9d9ba7076e6f3e166c462b358
esedgarcia <samu0850@gmail.com>
2025-02-10 22:39:51 -0500

    feat 🚀: Adding the Dockerfile

--- invitations-service/src/app.js
+++ invitations-service/src/app.js
@@ -4,9 +4,6 @@ import connectDB from './config/db.js';
 import invitationRoutes from './routes/invitationRoutes.js';
 import swaggerDocs from './config/swagger.js';
 
-// Add Swagger documentation to the app
-swaggerDocs(app);
-
 dotenv.config();
 connectDB();
 
@@ -14,6 +11,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/invitations', invitationRoutes);
+
+// Add Swagger documentation to the app
+swaggerDocs(app);
 
 const PORT = process.env.PORT || 4081;
 app.listen(PORT, () => console.log(`invitations-service server running on ${PORT}`));
